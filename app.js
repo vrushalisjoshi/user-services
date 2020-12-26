8a0e8b4b29ac7a207e9ee1fde396ede44135b4ec
vrushalisjoshi <vrushali.joshi99@gmail.com>
2020-12-26 16:51:32 +0530

    First Test API

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // This is our application entry point
+var express = require('express');
+var bodyParser = require("body-parser");
+var routes = require("./api-routes/routes.js");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+routes(app);
+
+var server = app.listen(3000, function () {
+    console.log("app running on port.", server.address().port);
+});
