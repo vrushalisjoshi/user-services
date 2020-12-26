5c6dc318b3bfd4b39fc2c7780a9661b33c56e9a0
vrushalisjoshi <vrushali.joshi99@gmail.com>
2020-12-26 17:41:02 +0530

    Testing API modifications

--- api-routes/routes.js
+++ api-routes/routes.js
@@ -1,7 +1,7 @@
 var appRouter = function (app) {
     app.get("/", function (req, res) {
-        res.status(200).send("Welcome to our restful API");
+        res.status(200).send("user-services API testing");
     });
-}
+};
 
 module.exports = appRouter;
--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(3000, function () {
-    console.log("app running on port.", server.address().port);
-});
+// var server = app.listen(3000, function () {
+//     console.log("app running on port.", server.address().port);
+// });
+
+module.exports = app;
